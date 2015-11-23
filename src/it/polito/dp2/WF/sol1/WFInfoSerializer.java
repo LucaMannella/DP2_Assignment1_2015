c86d627984596343797d9d07c24f0c25a05ef710
Luke <luca.mannella@outlook.it>
2015-11-24 00:39:15 +0100

    The files related to the part B were added to the repository.

---
+++ src/it/polito/dp2/WF/sol1/WFInfoSerializer.java
@@ -0,0 +1,13 @@
+package it.polito.dp2.WF.sol1;
+
+/**
+ * @author Luca (Sarcares)
+ */
+public class WFInfoSerializer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
