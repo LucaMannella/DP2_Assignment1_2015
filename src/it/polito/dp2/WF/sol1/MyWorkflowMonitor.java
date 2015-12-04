6c2b6bf55e5bf47ca406ea3c7c1c68f5c2d667a5
Luke <luca.mannella@outlook.it>
2015-12-04 03:46:58 +0100

    Inizio il telaio per la seconda parte dell'assignment 1b.

---
+++ src/it/polito/dp2/WF/sol1/MyWorkflowMonitor.java
@@ -0,0 +1,33 @@
+package it.polito.dp2.WF.sol1;
+
+import java.util.Set;
+
+import it.polito.dp2.WF.ProcessReader;
+import it.polito.dp2.WF.WorkflowReader;
+
+public class MyWorkflowMonitor implements it.polito.dp2.WF.WorkflowMonitor {
+	
+	public MyWorkflowMonitor() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+
+	@Override
+	public Set<ProcessReader> getProcesses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WorkflowReader getWorkflow(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<WorkflowReader> getWorkflows() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
