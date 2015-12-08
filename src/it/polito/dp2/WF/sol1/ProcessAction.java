313b05a2f65c81982e4a62bbef758f6c0fe8e8fe
Luke <luca.mannella@outlook.it>
2015-12-08 03:29:01 +0100

    The ProcessAction class was implemented

--- src/it/polito/dp2/WF/sol1/ProcessAction.java
+++ src/it/polito/dp2/WF/sol1/ProcessAction.java
@@ -1,8 +1,15 @@
 package it.polito.dp2.WF.sol1;
 
-import org.w3c.dom.Element;
+import java.util.HashSet;
 
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import it.polito.dp2.WF.ActionReader;
+import it.polito.dp2.WF.ProcessActionReader;
 import it.polito.dp2.WF.WorkflowReader;
+import it.polito.dp2.WF.util.WFAttributes;
+import it.polito.dp2.WF.util.WFElements;
 
 /**
  * This is a concrete implementation of abstract class AbstractActionReader (that implements the interface ActionReader).<BR>
@@ -13,11 +20,37 @@ import it.polito.dp2.WF.WorkflowReader;
  * 
  * @author Luca
  */
-public class ProcessAction extends AbstractActionReader {
+public class ProcessAction extends AbstractActionReader implements ProcessActionReader {
+
+	private WorkflowReader nextWorkflow;
 
 	public ProcessAction(Element action, WorkflowReader workflow) {
 		super(action, workflow);
-		// TODO Auto-generated constructor stub
+
+//TODO	if(action==null) return
+		
+		Element root = (Element) action.getParentNode().getParentNode();
+		NodeList workflows = root.getElementsByTagName(WFElements.WORKFLOW);
+		NodeList processes = root.getElementsByTagName(WFElements.PROCESS);
+		
+		Element processAction = (Element)action.getElementsByTagName(WFElements.PROCESS_ACTION).item(0);
+//		if(processAction == null) return;
+		String wfName = processAction.getAttribute(WFAttributes.ACTION_PROCESS_NEXT);
+		
+		//for each action I'm looking for its data
+		for(int i=0; i<workflows.getLength(); i++) {
+			Element wf = (Element)workflows.item(i);
+			if( wf.getAttribute(WFAttributes.WORKFLOW_NAME).equals(wfName) ) {
+				nextWorkflow = new ConcreteWorkflowReader(wf, processes);				
+				break;
+			}
+		}
+		
+	}
+
+	@Override
+	public WorkflowReader getActionWorkflow() {
+		return this.nextWorkflow;
 	}
 
 }
