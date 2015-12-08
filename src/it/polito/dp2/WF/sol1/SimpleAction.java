8e727b94fc89a9723a2828103573106f430ad24d
Luke <luca.mannella@outlook.it>
2015-12-08 03:28:38 +0100

    The SimpleAction class was implemented.

--- src/it/polito/dp2/WF/sol1/SimpleAction.java
+++ src/it/polito/dp2/WF/sol1/SimpleAction.java
@@ -1,8 +1,18 @@
 package it.polito.dp2.WF.sol1;
 
-import org.w3c.dom.Element;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import it.polito.dp2.WF.ActionReader;
+import it.polito.dp2.WF.SimpleActionReader;
 import it.polito.dp2.WF.WorkflowReader;
+import it.polito.dp2.WF.util.WFAttributes;
+import it.polito.dp2.WF.util.WFElements;
 
 /**
  * This is a concrete implementation of abstract class AbstractActionReader (that implements the interface ActionReader).<BR>
@@ -13,12 +23,56 @@ import it.polito.dp2.WF.WorkflowReader;
  * 
  * @author Luca
  */
-public class SimpleAction extends AbstractActionReader {
+public class SimpleAction extends AbstractActionReader implements SimpleActionReader {
+
+	private Set<ActionReader> nextActions;
 
 	public SimpleAction(Element action, WorkflowReader workflow) {
 		super(action, workflow);
 		
-		Element simpleAction = (Element) action.getElementsByTagName("simple_action").item(0);
+		nextActions = new HashSet<ActionReader>();
+//TODO:	if(action == null) return;
+		
+		Element simpleAction = (Element)action.getElementsByTagName(WFElements.SIMPLE_ACTION).item(0);
+//		if(simpleAction == null) return;
+		
+		String actionStrings = simpleAction.getAttribute(WFAttributes.ACTION_SIMPLE_NEXT);
+		//taking all the strings related to next actions and check if they exists
+		if( !actionStrings.equals("") ) {
+			//taking the list of all the actions inside the workflow 
+			Element wf = (Element)action.getParentNode();
+			NodeList actions = wf.getElementsByTagName(WFElements.ACTION);
+			
+			//dividing the string in the different ids
+			StringTokenizer st = new StringTokenizer(actionStrings, " ");
+			while(st.hasMoreTokens()) {
+				String act = st.nextToken();
+				
+				//for each action I'm looking for its data
+				for(int i=0; i<actions.getLength(); i++) {
+					Element azione = (Element)actions.item(i);
+					if( azione.getAttribute(WFAttributes.ACTION_ID).equals(act) ) {
+						
+						ActionReader ar;
+						if( azione.getElementsByTagName(WFElements.PROCESS_ACTION).getLength() >= 1 )	//"process_action"
+							ar = new ProcessAction(azione, workflow);
+						else
+							ar = new SimpleAction(azione, workflow);
+						
+						nextActions.add(ar);
+						//when I found the match I go the next token
+						break;
+					}
+				}//end for on Element Actions
+				
+			}//end while on tokens (Actions ID)
+		}//end of if 
+		
+	}
+
+	@Override
+	public Set<ActionReader> getPossibleNextActions() {
+		return this.nextActions;
 	}
 
 }
