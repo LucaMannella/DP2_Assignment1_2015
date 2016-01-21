20fee40813464c903fecaf7fcc14ac394c6be12a
Luke <luca.mannella@outlook.it>
2016-01-21 20:12:49 +0100

    The parser is now usable for testing.

--- src/it/polito/dp2/WF/myTests/WFParser.java
+++ src/it/polito/dp2/WF/myTests/WFParser.java
@@ -1,79 +1,32 @@
 package it.polito.dp2.WF.myTests;
 
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
-
 import it.polito.dp2.WF.WorkflowMonitor;
 import it.polito.dp2.WF.WorkflowMonitorException;
 import it.polito.dp2.WF.WorkflowMonitorFactory;
-import it.polito.dp2.WF.sol1.util.DomParseErrorHandler;
 
 public class WFParser {
 	
-	private WorkflowMonitor monitor;
-	private DocumentBuilder builder;
-	private DateFormat dateFormat;
+	final static String filename="dtd/wfInfo.xml";
 
-	public WFParser(String inputFileName) 
-			throws WorkflowMonitorException, ParserConfigurationException, SAXException, IOException {
-		
-		WorkflowMonitorFactory WFfactory = WorkflowMonitorFactory.newInstance();
-		monitor = WFfactory.newWorkflowMonitor();
-		
-		builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-		builder.setErrorHandler( new DomParseErrorHandler() );
-		builder.parse(inputFileName);
-		
-		// This element will help to managing the data format
-		dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss:SS z");
-		//dateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss:MM z");	//z for timezone and MM for millis
-	}
-	
-	
 	public static void main(String[] args) {
-		if((args.length != 1) || (args[0]==null)) {
-			System.err.println("Error! Usage: <program_name> <output.xml>");
-			System.err.println("args.length is equal to "+args.length);
-			return;
-		}
 		
+		//DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss:SS z");
+		
+		//set system property
+		System.setProperty("it.polito.dp2.WF.sol1.WFInfo.file", filename);
+		System.setProperty("it.polito.dp2.WF.WorkflowMonitorFactory", "it.polito.dp2.WF.sol1.WorkflowMonitorFactory");
+		
+		WorkflowMonitor monitor = null;
 		try {
-			WFParser p = new WFParser(args[0]);
+			monitor = WorkflowMonitorFactory.newInstance().newWorkflowMonitor();
 		}
 		catch (WorkflowMonitorException e) {
-			System.err.println("Could not instantiate the manager class: "+e.getMessage());
+			System.err.println("Error: "+e.getMessage());
 			e.printStackTrace();
 			System.exit(1);
 		}
-		catch (IOException ioe) {
-			// I/O error
-			System.err.println("Unable to open/read the file");
-			System.exit(11);
-		}
-		catch (SAXParseException spe) {	
-			// Parsing error
-			System.err.println("\n** Parsing error"+", line "+spe.getLineNumber()+", uri "+spe.getSystemId());
-			System.err.println("\t"+spe.getMessage());
-			System.exit(21);
-		}
-		catch (SAXException se) {
-			System.err.println("\n** Internal error.");
-			System.err.println("\t"+se.getMessage());
-			System.exit(22);
-		}
-		catch (ParserConfigurationException pce) {
-			System.err.println("\n** Internal error.");
-			System.err.println("\t Parser with specified options can't be built");
-			System.exit(23);
-		} 
+		
+		System.out.println(monitor);
 	}
 
 }
